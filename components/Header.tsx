705e0af3f9f246fdccf908375c24c8054f0bc75d
sabrexghosh@gmail.com <96774134+Night3y3@users.noreply.github.com>
2023-11-12 17:05:51 +0530

    button-fe-login-and-signup

---
+++ components/Header.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
+
+interface HeaderProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({
+    children,
+    className
+}) => {
+    const router = useRouter();
+    const handleLogout = () => {
+        //Handl Logout
+    }
+    return (
+        <div className={twMerge(`h-fit bg-gradient-to-b from-emerald-800 p-6`, className)}>
+
+            <div className="w-full mb-4 flex items-center justify-between">
+                <div className=" hidden md:flex gap-x-2 items-center">
+                    <button onClick={() => router.back()} className=" rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                        <RxCaretLeft size={35} className="text-white" />
+                    </button>
+                    <button onClick={() => router.forward()} className=" rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                        <RxCaretRight size={35} className="text-white" />
+                    </button>
+
+                </div>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button className="p-2 rounded-full bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <HiHome size={20} className="text-black" />
+                    </button>
+                    <button className="p-2 rounded-full bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <BiSearch size={20} className="text-black" />
+                    </button>
+                </div>
+                <div className="flex justify-between items-center gap-x-4">
+                    <>
+                        <div>
+                            <Button className=" bg-transparent text-neutral-300 font-medium">
+                                Sign up
+                            </Button>
+
+                        </div>
+                        <div>
+                            <Button className=" bg-white px-6 py-2">
+                                Log In
+                            </Button>
+
+                        </div>
+                    </>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
