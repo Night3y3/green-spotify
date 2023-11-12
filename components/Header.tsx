8d3e4e0b0d3de886202091b84b38aee5df4dbf24
sabrexghosh@gmail.com <96774134+Night3y3@users.noreply.github.com>
2023-11-13 02:55:16 +0530

    Layout done and gitignore

--- components/Header.tsx
+++ components/Header.tsx
@@ -43,20 +43,20 @@ const Header: React.FC<HeaderProps> = ({
                 <div className="flex justify-between items-center gap-x-4">
                     <>
                         <div>
-                            <Button className=" bg-transparent text-neutral-300 font-medium">
+                            <Button onClick={() => { }} className=" bg-transparent text-neutral-300 font-medium">
                                 Sign up
                             </Button>
 
                         </div>
                         <div>
-                            <Button className=" bg-white px-6 py-2">
+                            <Button onClick={() => { }} className=" bg-white px-6 py-2">
                                 Log In
                             </Button>
-
                         </div>
                     </>
                 </div>
             </div>
+            {children}
         </div>
     );
 }
