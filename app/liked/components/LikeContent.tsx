b362c9426fc684b0c71ce020561be26daa942429
sabrexghosh@gmail.com <96774134+Night3y3@users.noreply.github.com>
2023-11-25 19:55:21 +0530

    Like song page done and working

---
+++ app/liked/components/LikeContent.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import LikedButton from "@/components/LikedButton";
+import MediaItem from "@/components/MediaItem";
+import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+interface LikedContentProps {
+    songs: Song[];
+}
+
+const LikedContent: React.FC<LikedContentProps> = ({
+    songs
+}) => {
+    const router = useRouter();
+    const { isLoading, user } = useUser();
+
+    useEffect(() => {
+        if (!user && !isLoading) {
+            router.push("/");
+        }
+
+    }, [isLoading, user, router]);
+
+    if (songs.length === 0) {
+        return (
+            <div className="flex flex-col items-center justify-center h-full">
+                <h1 className="text-white text-4xl font-bold">
+                    You haven&apos;t liked any songs yet
+                </h1>
+                <p className="text-white text-sm mt-2">
+                    Like songs to see them here
+                </p>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <div className=" flex flex-col gap-y-2 w-full p-6">
+                {songs.map((song) => (
+                    <div
+                        key={song.id}
+                        className="flex items-center justify-between gap-x-4 w-full text-white hover:bg-neutral-800 p-4 rounded-lg transition"
+                    >
+                        <div className="flex-1">
+                            <MediaItem
+                                onClick={() => { }}
+                                data={song}
+                            />
+
+                        </div>
+                        <LikedButton songId={song.id} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default LikedContent;
