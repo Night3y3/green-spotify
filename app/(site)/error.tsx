08efa20acec78a9c109812daabd5911182cb7739
sabrexghosh@gmail.com <96774134+Night3y3@users.noreply.github.com>
2023-11-30 02:55:33 +0530

    custom error page

---
+++ app/(site)/error.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Errot = () => {
+    return (
+        <Box className=" h-full flex items-center justify-center">
+            <div className=" text-neutral-400">
+                Problem happened
+            </div>
+        </Box>
+    );
+}
+
+export default Errot;
